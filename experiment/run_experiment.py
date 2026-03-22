9f4b3a8d07a5e9a1919f34536056b54f6b51ef88
Leandru Martin <leandrumartin@gmail.com>
2026-03-22 18:14:20 -0500

    Shorten arguments

--- experiment/run_experiment.py
+++ experiment/run_experiment.py
@@ -15,7 +15,7 @@ for run_number, run in enumerate(experiment_runs):
         capture_output=True
     )
     subprocess.run(
-        f"../src/iperf3 -c localhost -p 5201 {'' if run['Protocol'] == 'TCP' else '--' + run['Protocol']} --json -t {run['Duration (s)']} -P {run['Parallelism Level']} -b {run['Bandwidth cap (bitrate, Gbps)'] * 1_000_000_000} --logfile ./output/run_{run_number}.log",
+        f"../src/iperf3 -c localhost {'' if run['Protocol'] == 'TCP' else '--' + run['Protocol']} -J -t {run['Duration (s)']} -P {run['Parallelism Level']} -b {run['Bandwidth cap (bitrate, Gbps)'] * 1_000_000_000} --logfile out/run_{run_number}.log",
         shell=True,
         capture_output=True
 )
