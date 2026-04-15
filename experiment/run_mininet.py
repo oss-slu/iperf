0c9ce2bc13f185fd97dbaccbec8ae0f6a513665b
Leandru Martin <leandrumartin@gmail.com>
2026-04-15 12:05:02 -0500

    Add configurable topology

--- experiment/run_mininet.py
+++ experiment/run_mininet.py
@@ -1,9 +1,38 @@
 from mininet.net import Mininet
-from mininet.topolib import TreeTopo
+from mininet.topo import *
 from mininet.node import OVSBridge
+import argparse
 
-tree2 = TreeTopo(depth=2,fanout=2)
-net = Mininet(topo=tree2, switch=OVSBridge, controller=None)
+parser = argparse.ArgumentParser(description="Run iperf3 experiments in Mininet.")
+parser.add_argument("--topo", help="""
+                        linear|minimal|reversed|single|torus|tree[,param=value
+                        ...] minimal=MinimalTopo linear=LinearTopo
+                        reversed=SingleSwitchReversedTopo
+                        single=SingleSwitchTopo tree=TreeTopo torus=TorusTopo""", default="minimal")
+parser.add_argument("--topo-options", help="Comma-separated list of key=value pairs to pass as options to the topology class. For example: --topo-options depth=2,fanout=2")
+args = parser.parse_args()
+
+match args.topo:
+    case "linear":
+        topo_cls = LinearTopo
+    case "minimal":
+        topo_cls = MinimalTopo
+    case "reversed":
+        topo_cls = SingleSwitchReversedTopo
+    case "single":
+        topo_cls = SingleSwitchTopo
+    case "tree":
+        topo_cls = TreeTopo
+    case "torus":
+        topo_cls = TorusTopo
+    case _:
+        raise ValueError(f"Unknown topology: {args.topo}")
+
+topo_options = dict(option.split('=') for option in args.topo_options.split(',')) if args.topo_options else {}
+topo_options = {key: int(value) for key, value in topo_options.items()}  # Convert values to integers
+tree = topo_cls(**topo_options)
+
+net = Mininet(topo=tree, switch=OVSBridge, controller=None)
 net.start()
 h1, h2  = net.hosts[0], net.hosts[1]
 print(h1.cmd('../src/iperf3 -s &'))
