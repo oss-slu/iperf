76f6d9b9bd185ea5f1ea1710d7c350ac3aafdce8
Leandru Martin <leandrumartin@gmail.com>
2026-04-18 10:33:10 -0500

    Make output directories if they don't exist

--- experiment/run_experiment.py
+++ experiment/run_experiment.py
@@ -1,6 +1,7 @@
 import subprocess
 import csv
 import argparse
+from os import path, makedirs
 
 parser = argparse.ArgumentParser(description="Run iperf3 experiments based on a CSV configuration file.")
 parser.add_argument("ip", help="The IP address of the iperf3 server to connect to.")
@@ -20,8 +21,13 @@ for run_number, run in enumerate(experiment_runs):
         shell=True,
         capture_output=True
     )
+
+    output_dir = f"out/{args.out_subdir}/{run['Protocol']}"
+    if not path.exists(output_dir):
+        makedirs(output_dir)
+
     subprocess.run(
-        f"../src/iperf3 -c {args.ip} {'' if run['Protocol'] == 'tcp' else '--' + run['Protocol']} -J -t {run['Duration (s)']} -P {run['Parallelism Level']} -b {str(int(run['Bandwidth cap (bitrate, Gbps)']) * 1_000_000_000)} --logfile out/{args.out_subdir}/{run['Protocol']}/run{run_number}_dur{run['Duration (s)']}_p{run['Parallelism Level']}_b{run['Bandwidth cap (bitrate, Gbps)']}_delay{run['Delay (ms)']}_l{run['Loss (%)']}.log",
+        f"../src/iperf3 -c {args.ip} {'' if run['Protocol'] == 'tcp' else '--' + run['Protocol']} -J -t {run['Duration (s)']} -P {run['Parallelism Level']} -b {str(int(run['Bandwidth cap (bitrate, Gbps)']) * 1_000_000_000)} --logfile {output_dir}/run{run_number}_dur{run['Duration (s)']}_p{run['Parallelism Level']}_b{run['Bandwidth cap (bitrate, Gbps)']}_delay{run['Delay (ms)']}_l{run['Loss (%)']}.log",
         shell=True,
         capture_output=True
     )
