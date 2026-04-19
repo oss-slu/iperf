0bba0bfa874c00e1dd714f6f14fba870ff43f7ac
Leandru Martin <leandrumartin@gmail.com>
2026-04-19 18:39:54 -0500

    Add Mininet performance evaluation files

---
+++ experiment/run_mininet_evaluation.py
@@ -0,0 +1,33 @@
+import subprocess
+import csv
+import argparse
+from os import path, makedirs
+
+parser = argparse.ArgumentParser(description="Run iperf3 experiments based on a CSV configuration file.")
+parser.add_argument("ip", help="The IP address of the iperf3 server to connect to.")
+parser.add_argument("out_subdir", help="The subdirectory within the 'out' directory where log files will be saved.")
+args = parser.parse_args()
+
+experiment_runs = []
+
+with open("performance_evaluation.csv", "r", newline="") as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        experiment_runs.append(row)
+
+for run_number, run in enumerate(experiment_runs):
+    subprocess.run(
+        f"tc qdisc add dev lo root netem delay {run['Delay (ms)']}ms",
+        shell=True,
+        capture_output=True
+    )
+
+    output_dir = f"out/{args.out_subdir}/{run['Protocol']}"
+    if not path.exists(output_dir):
+        makedirs(output_dir)
+
+    subprocess.run(
+        f"../src/iperf3 -c {args.ip} {'' if run['Protocol'] == 'tcp' else '--' + run['Protocol']} -J -P {run['Parallelism Level']} -b {str(int(run['Bandwidth cap (bitrate, Gbps)']) * 1_000_000_000)} --logfile {output_dir}/run{run_number}_p{run['Parallelism Level']}_b{run['Bandwidth cap (bitrate, Gbps)']}_delay{run['Delay (ms)']}.log",
+        shell=True,
+        capture_output=True
+    )
---
+++ experiment/run_mininet_performance_evaluation.py
@@ -0,0 +1,44 @@
+from mininet.net import Mininet
+from mininet.topo import *
+from mininet.topolib import TreeTopo, TorusTopo
+from mininet.node import OVSBridge
+import argparse
+import csv
+
+experiment_runs = []
+
+with open("mininet_performance_evaluation.csv", "r", newline="") as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        experiment_runs.append(row)
+
+for run_number, run in enumerate(experiment_runs):
+    match run["Topology"]:
+        case "linear":
+            topo_cls = LinearTopo
+        case "minimal":
+            topo_cls = MinimalTopo
+        case "reversed":
+            topo_cls = SingleSwitchReversedTopo
+        case "single":
+            topo_cls = SingleSwitchTopo
+        case "tree":
+            topo_cls = TreeTopo
+        case "torus":
+            topo_cls = TorusTopo
+        case _:
+            raise ValueError(f"Unknown topology: {run['Topology']}")
+
+    topo_options = dict(option.split('=') for option in run['Topology options'].split(',')) if run['Topology options'] else {}
+    topo_options = {key: int(value) for key, value in topo_options.items()}  # Convert values to integers
+    topo = topo_cls(**topo_options)
+
+    out_subdir = f"{run['Topology']}_{'_'.join(f'{key}{value}' for key, value in topo_options.items())}"
+
+    net = Mininet(topo=topo, switch=OVSBridge, controller=None)
+    net.start()
+    h1 = net.hosts[0]
+    print(h1.cmd('../src/iperf3 -s &'))
+    for host in net.hosts[1:]:
+        print(host.cmd(f'python3 run_mininet_evaluation.py {h1.IP()} {out_subdir} &'))
+    net.stop()
